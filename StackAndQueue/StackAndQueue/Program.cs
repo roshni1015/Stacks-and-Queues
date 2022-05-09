773a139929c1e0d34c4b9148ebb31d4cb1bfb739
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-09 13:16:17 +0530

    Pushing Values

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using StackAndQueue;
+
 Console.WriteLine("Welcome to Stack And Queue!");
+StackUsingLinkList value = new StackUsingLinkList();
+value.push(70);
+value.push(30);
+value.push(56);
+Console.Write("Satck Values are ");
+value.display();
+
 
---
+++ StackAndQueue/StackAndQueue/StackUsingLinkList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class StackUsingLinkList
+    {
+
+        Node top;
+        public StackUsingLinkList()
+        {
+            this.top = null;
+
+        }
+        public void push(int x)
+        {
+            Node temp = new Node();
+            if (temp == null)
+            {
+                Console.WriteLine("Heap overflow");
+                return;
+            }
+            else
+            {
+                temp.data = x;
+                temp.link = top;
+                top = temp;
+            }
+
+        }
+        public bool isEmpty()
+        {
+            return top == null;
+        }
+        public void display()
+        {
+            if (top == null)
+            {
+                Console.Write("\nStack Underflow");
+                return;
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+
+                    Console.Write("{0}->", temp.data);
+                    temp = temp.link;
+                }
+            }
+        }
+    }
+}
